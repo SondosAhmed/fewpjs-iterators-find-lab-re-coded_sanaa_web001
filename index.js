457f8ab8a58f7fbb386ffa847f6fcdb11c1b38d5
Sondos Ahmed <sondosalshami@gmail.com>
2020-08-27 17:54:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-const testVar = {}
-
-function testFunc() {
-  return "hi"
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+  //...
+]
+function testFunc(record) {
+  let f = record.find(e => e.result == "W");
+  return f.year;
 }
